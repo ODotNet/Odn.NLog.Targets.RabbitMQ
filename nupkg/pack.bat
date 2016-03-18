dd3b6b7f322aac59d5c6783207da25aec8396e30
DukeCheng <flyear@flyear.net>
2016-03-18 15:20:15 +0800

    added missing files

--- nupkg/pack.bat
+++ nupkg/pack.bat
@@ -1,3 +1,4 @@
 del *.nupkg
 "..\src\.nuget\NuGet.exe" "pack" "..\src\NLog.Targets.RabbitMQ\NLog.Targets.RabbitMQ.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
 pause
+for /f %%i in ('dir *.nupkg /b') do nuget.exe push %%i -s http://nuget.ymc.int/ yaomaiche.com
